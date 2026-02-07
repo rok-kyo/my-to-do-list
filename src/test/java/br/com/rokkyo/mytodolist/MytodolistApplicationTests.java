c83531586ffdf3d443fc2cf2df3fd37e3a61501e
rok-kyo <caiodotrocha@gmail.com>
2026-02-07 00:31:44 -0300

    Primeiro commit, inicializando projeto

---
+++ src/test/java/br/com/rokkyo/mytodolist/MytodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rokkyo.mytodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
