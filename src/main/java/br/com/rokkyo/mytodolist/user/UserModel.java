b1726f0fb79275b5a28c83a0ccede13dc0b47639
rok-kyo <caiodotrocha@gmail.com>
2026-02-08 23:21:23 -0300

    feat: Adiciona o lombok

--- src/main/java/br/com/rokkyo/mytodolist/user/UserModel.java
+++ src/main/java/br/com/rokkyo/mytodolist/user/UserModel.java
@@ -1,32 +1,11 @@
 package br.com.rokkyo.mytodolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     private String username;
     private String name;
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
